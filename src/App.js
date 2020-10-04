ec1aa74f52cf1c3719dd29b71106baf7daef8dac
Christopher Girvin <bugbountychrisg8691@gmail.com>
2020-10-04 17:37:59 -0400

    added parameter specs for reference

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ const API_KEY = "I83MwsFqeOeinDpocUImQQWOw3eDdrbZsjxt6636"
 // example of api call
 // https://api.nasa.gov/planetary/apod?api_key=I83MwsFqeOeinDpocUImQQWOw3eDdrbZsjxt6636
 // check rate limit in header under X-RateLimit-Limit and X-RateLimit-Remaining 
+// Parameter 	Type 	      Default 	Description
+// date	      YYYY-MM-DD	today	    The date of the APOD image to retrieve
+// hd	        bool	      False	    Retrieve the URL for the high resolution image
+// api_key	  string	    DEMO_KEY	api.nasa.gov key for expanded usage
 
 function App() {
   return (
