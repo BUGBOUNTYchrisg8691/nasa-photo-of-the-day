b7d17670f30dae9940c6c9b6518bb0ee92e6de83
Christopher Girvin <bugbountychrisg8691@gmail.com>
2020-10-04 17:39:31 -0400

    updated api key

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 
 const API_KEY = "I83MwsFqeOeinDpocUImQQWOw3eDdrbZsjxt6636"
+const URL = `https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`
 // example of api call
 // https://api.nasa.gov/planetary/apod?api_key=I83MwsFqeOeinDpocUImQQWOw3eDdrbZsjxt6636
 // check rate limit in header under X-RateLimit-Limit and X-RateLimit-Remaining 
